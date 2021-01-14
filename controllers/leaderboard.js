c7c49967ffb40c4d22958b871106b850e8e16bd6
GSWes <wesley.hedrick@gmail.com>
2021-01-14 10:19:02 -0500

    worked on save Progress and quiz Feedback pages

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,21 +1,25 @@
-const Leaderboard = require("../models/leaderboard");
-
+const {Leaderboard} = require('../models');
 
 const leaderboard = async (req, res) => {
-    // console.log('dir', process.cwd()) 
-    
-  const leaderboardData = await Leaderboard().findAll();
-    
+    // console.log('dir', process.cwd())     
+  const leaderboardData = await Leaderboard.findAll();
+
+  console.log(leaderboardData);
+//   res.json(leaderboardData)
+//   return
   res.render('leaderboard', {
       locals: {
           leaderboard: leaderboardData, 
       },
+      
       partials: {
           header: process.cwd() + '/templates/partials/header.html',
           footer: process.cwd() + '/templates/partials/footer.html'
         }
     }); // process.cwd()gives root directory of entire project
 };
+
+
 module.exports = {
   leaderboard,
 };
