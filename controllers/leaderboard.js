ac241972c58a607129793a9ec120d52a5097a057
davidvaughan86 <david.vaughan86@gmail.com>
2021-01-13 20:29:16 -0500

    added config sequelize, debugged leaderboard controller and template

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,5 +1,21 @@
-const leaderboard = (req, res) => {res.send('leaderboard')}
+const Leaderboard = require("../models/leaderboard");
 
+
+const leaderboard = async (req, res) => {
+    // console.log('dir', process.cwd()) 
+    
+  const leaderboardData = await Leaderboard().findAll();
+    
+  res.render('leaderboard', {
+      locals: {
+          leaderboard: leaderboardData, 
+      },
+      partials: {
+          header: process.cwd() + '/templates/partials/header.html',
+          footer: process.cwd() + '/templates/partials/footer.html'
+        }
+    }); // process.cwd()gives root directory of entire project
+};
 module.exports = {
-    leaderboard
-}
+  leaderboard,
+};
