5c9d978e54a69b07413f554dfed4b895d5ad6cba
davidvaughan86 <david.vaughan86@gmail.com>
2021-01-14 21:10:59 -0500

    did the best i could on leaderboard styling before i got too tired

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,15 +1,18 @@
 const {Leaderboard} = require('../models');
+const {Users} = require('../models');
 
 const leaderboard = async (req, res) => {
     // console.log('dir', process.cwd())     
   const leaderboardData = await Leaderboard.findAll();
+  const ussernameData = await Users.findAll();
 
   console.log(leaderboardData);
 //   res.json(leaderboardData)
 //   return
   res.render('leaderboard', {
       locals: {
-          leaderboard: leaderboardData, 
+          leaderboard: leaderboardData,
+          leaderboard: ussernameData, 
       },
       
       partials: {
